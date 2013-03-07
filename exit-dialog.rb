bf3423e6fd3bd4a338169b1dd90775ea450336b0
Miguel <miguel837@gmail.com>
2013-03-07 19:32:45 +0100

    correct indentation

--- exit-dialog.rb
+++ exit-dialog.rb
@@ -17,37 +17,34 @@ class ExitDialog < Gtk::Window
     button_box =  Gtk::HBox.new
     add button_box
 
-	  hibernate_button = Gtk::Button.new "Hibernate"	  
-	  hibernate_button.signal_connect "clicked" do 
-	  	system 'dbus-send --print-reply --system --dest=org.freedesktop.Hal /org/freedesktop/Hal/devices/computer org.freedesktop.Hal.Device.SystemPowerManagement.Hibernate'
-    	Gtk.main_quit 
-		end
-	  button_box.add hibernate_button
+    hibernate_button = Gtk::Button.new "Hibernate"	  
+    hibernate_button.signal_connect "clicked" do 
+        system 'dbus-send --print-reply --system --dest=org.freedesktop.Hal /org/freedesktop/Hal/devices/computer org.freedesktop.Hal.Device.SystemPowerManagement.Hibernate'
+        Gtk.main_quit 
+    end
+    button_box.add hibernate_button
 
-	  suspend_button = Gtk::Button.new "Suspend"
-	  suspend_button.signal_connect "clicked" do 
-    	system 'dbus-send --system --print-reply --dest=org.freedesktop.UPower /org/freedesktop/UPower org.freedesktop.UPower.Suspend' 
-    	Gtk.main_quit
-		end
-		button_box.add suspend_button
+    suspend_button = Gtk::Button.new "Suspend"
+    suspend_button.signal_connect "clicked" do 
+        system 'dbus-send --system --print-reply --dest=org.freedesktop.UPower /org/freedesktop/UPower org.freedesktop.UPower.Suspend' 
+        Gtk.main_quit
+    end
+    button_box.add suspend_button
 
-	  suspend_button = Gtk::Button.new "Reboot"
-	  suspend_button.signal_connect "clicked" do 
-    	system 'dbus-send --print-reply --system --dest=org.freedesktop.Hal /org/freedesktop/Hal/devices/computer org.freedesktop.Hal.Device.SystemPowerManagement.Reboot' 
-    	Gtk.main_quit
-		end
-		button_box.add suspend_button
+    suspend_button = Gtk::Button.new "Reboot"
+    suspend_button.signal_connect "clicked" do 
+        system 'dbus-send --print-reply --system --dest=org.freedesktop.Hal /org/freedesktop/Hal/devices/computer org.freedesktop.Hal.Device.SystemPowerManagement.Reboot' 
+    end
+    button_box.add suspend_button
 
-	  suspend_button = Gtk::Button.new "Shutdown"
-	  suspend_button.signal_connect "clicked" do 
-    	system 'dbus-send --print-reply --system --dest=org.freedesktop.Hal /org/freedesktop/Hal/devices/computer org.freedesktop.Hal.Device.SystemPowerManagement.Shutdown' 
-    	Gtk.main_quit
-		end
-		button_box.add suspend_button
+    suspend_button = Gtk::Button.new "Shutdown"
+    suspend_button.signal_connect "clicked" do 
+        system 'dbus-send --print-reply --system --dest=org.freedesktop.Hal /org/freedesktop/Hal/devices/computer org.freedesktop.Hal.Device.SystemPowerManagement.Shutdown' 
+    end
+    button_box.add suspend_button
 
-
-	  show_all
-  end
+    show_all
+    end
 end
 
 Gtk.init
